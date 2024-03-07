468d71db42492ec431e98d6d8e640199e9851ad2
appmovilco <appmovilco@gmail.com>
2024-03-06 22:57:05 -0500

    Ajuste basico de main

--- captura_informacion_manual/main.py
+++ captura_informacion_manual/main.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
 from flask import Flask, jsonify
 from api.controller.house import house_api
 from out.adapter.db.postgres import create_tables
-
-loaded = load_dotenv('.env.development')
 
 app = Flask(__name__)
 app.register_blueprint(house_api)
